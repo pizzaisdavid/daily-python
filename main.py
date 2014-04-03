0e30252b6deacdd4da3227d8f81db0c5d6edc444
pizzaisdavid <davidispizza@gmail.com>
2014-04-03 15:42:43 -0400

    generates random colors

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pygame
+import random
 from pygame import gfxdraw
 
-window = pygame.display.set_mode((640, 480))
-color = pygame.Color(0, 255, 0)
-for i in range(600):
-    gfxdraw.pixel(window, i, i, color)
-    pygame.display.update()
+def generate_random_color():
+    color = []
+    for i in range(3):
+        color.append(random.randrange(0, 255))
+    return color
 
+def generate_colors(number):
+    colors = []
+    while len(colors) < number:
+        random_color = generate_random_color()
+        if random_color not in colors:
+            colors.append(random_color)
+    return colors
+
+window = pygame.display.set_mode((640, 480))
+number_of_colors = 30
+colors = generate_colors(number_of_colors)
+# pygame.Color(0, 255, 0)
+print colors
