ef9964a831af7e717f89df1a07913613c43c8f13
pizzaisdavid <davidispizza@gmail.com>
2014-04-03 15:17:34 -0400

    pygame test

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import pygame
+from pygame import gfxdraw
+
+window = pygame.display.set_mode((640, 480))
+color = pygame.Color(0, 255, 0)
+for i in range(600):
+    gfxdraw.pixel(window, i, i, color)
+    pygame.display.update()
 
