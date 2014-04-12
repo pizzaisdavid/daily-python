3d9828fef701f88bc84ac42ed28916edc5db1cb7
pizzaisdavid <davidispizza@gmail.com>
2014-04-12 19:53:06 -0400

    Update occurrences.py

--- occurrences.py
+++ occurrences.py
@@ -1,8 +1,8 @@
-def occurrences(iterable, find):
-    '''Return all occurrences of find in iterable.'''
-    # return [i for i, x in enumerate(iterable) if x == find]
+def occurrences(iterable, item):
+    '''Return all occurrences of item in iterable.'''
+    # return [i for i, x in enumerate(iterable) if x == item]
     indices = []
     for i, x in enumerate(iterable):
-        if x == find:
+        if x == item:
             indices.append(i)
     return indices
