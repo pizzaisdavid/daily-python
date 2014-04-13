1809b09b7071c3f30ba5f7755fca7ca1929446f4
pizzaisdavid <davidispizza@gmail.com>
2014-04-13 11:24:21 -0400

    Update gorellian_alphabet_sort.py

--- gorellian_alphabet_sort.py
+++ gorellian_alphabet_sort.py
@@ -1,1 +1,8 @@
-def 
+def initialize_variables(filename):
+    
+
+def contains(iterable, required):
+    for x in required:
+        if x not in iterable:
+            return False
+    return True
