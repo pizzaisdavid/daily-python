5cca12c2ebfee8fb8252959647a5bc385635c02b
pizzaisdavid <davidispizza@gmail.com>
2014-04-12 22:30:09 -0400

    Update occurrences.py

--- occurrences.py
+++ occurrences.py
@@ -1,7 +1,5 @@
 def occurrences(iterable, item):
-    '''
-    Return all occurrences of item in iterable as a list.
-    '''
+    '''Find all occurrences and return the indices in a list.'''
     # return [i for i, x in enumerate(iterable) if x == item]
     indices = []
     for i, x in enumerate(iterable):
