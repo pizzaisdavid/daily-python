c7eef36eab1950866337f13fa6f235bbfdd3302b
pizzaisdavid <davidispizza@gmail.com>
2014-04-12 21:46:57 -0400

    Update the_winning_move_x_games_edition.py

--- the_winning_move_x_games_edition.py
+++ the_winning_move_x_games_edition.py
@@ -1,4 +1,12 @@
 def show_winning_move(filename):
+    '''.'''
+    def initilaize_variables(filename):
+        '''.'''
+        content = open(filename).readlines()
+        text = list(''.join(content).replace('\n', ''))
+        player = text[0]
+        board = text[1:]
+        return player, board
   
   
   
