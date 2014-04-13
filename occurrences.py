9692eec901471fb2ffed9ec7dfcdf26272527f3e
pizzaisdavid <davidispizza@gmail.com>
2014-04-12 22:10:03 -0400

    docstring grammar

--- occurrences.py
+++ occurrences.py
@@ -1,6 +1,6 @@
 def occurrences(iterable, item):
     '''
-    Returns all occurrences of item in iterable as a list.
+    Return all occurrences of item in iterable as a list.
     '''
     # return [i for i, x in enumerate(iterable) if x == item]
     indices = []
