a36ba008b47ea0bf276f2e06d0e64c37effeb0e4
pizzaisdavid <davidispizza@gmail.com>
2014-04-12 23:07:14 -0400

    SOLVED

--- longest_two_character_substring.py
+++ longest_two_character_substring.py
@@ -1,25 +1,24 @@
 def longest_two_character_substring(string):
-    '''.'''
     def longest(iterable):
         longest = ''
         for item in iterable:
             if len(longest) < len(item):
                 longest = item
         return longest
-        
+    
     def occurrences(iterable, item):
-        '''Return all occurrences of item in iterable as a list.'''
-        # return [i for i, x in enumerate(iterable) if x == item]
         array = ['']
         for x in iterable:
             if x in item:
-                array[:-1] = array[:-1] + x
-            else:
+                array[-1] = array[-1] + x
+            elif array[-1] != '':
                 array.append('')
+        if array[-1] == '':
+            array.pop(-1)
         return array
     
     list_of_strings = []
-    for i, character in enumerate(string[:-1]):
-        letters = string[i: i + 1]
-        list_of_strings = occurrences(string, letters)
-        
+    for i, c in enumerate(string[:-1]):
+        letters = string[i: i + 2]
+        list_of_strings.extend(occurrences(string, letters))
+    print longest(list_of_strings)
