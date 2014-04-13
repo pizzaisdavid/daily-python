37b09a70332118f3032a4077203506302efca8af
pizzaisdavid <davidispizza@gmail.com>
2014-04-13 11:55:09 -0400

    Update gorellian_alphabet_sort.py

--- gorellian_alphabet_sort.py
+++ gorellian_alphabet_sort.py
@@ -1,5 +1,9 @@
 def initialize_variables(filename):
-    
+    CONTENT = open(filename).readlines()
+    TEXT = list(''.join(CONTENT).replace('\n', ''))
+    word_count = TEXT[0]
+    board = TEXT[1:]
+    return player, board
 
 def contains(iterable, required):
     for x in required:
