01e89dc7ba9fedf0b19124defd5e91c511124603
pizzaisdavid <davidispizza@gmail.com>
2014-04-12 22:00:06 -0400

    Update the_winning_move_x_games_edition.py

--- the_winning_move_x_games_edition.py
+++ the_winning_move_x_games_edition.py
@@ -1,12 +1,14 @@
 def show_winning_move(filename):
-    '''.'''
-    def initilaize_variables(filename):
+    '''If there is a winning move, find and print it.'''
+    def initialize_variables(filename):
         '''.'''
         content = open(filename).readlines()
         text = list(''.join(content).replace('\n', ''))
         player = text[0]
         board = text[1:]
         return player, board
+        
+    player, board = initialize_variables(filename)
   
   
   
