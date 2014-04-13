c855d46815645eb6923000e61effba6980967041
pizzaisdavid <davidispizza@gmail.com>
2014-04-12 23:40:16 -0400

    Update longest_two_character_substring.py

--- solved/longest_two_character_substring.py
+++ solved/longest_two_character_substring.py
@@ -14,8 +14,9 @@ def longest_two_character_substring(string):
     for i, c in enumerate(string[:-1]):
         letters = string[i: i + 2]
         list_of_strings.extend(occurrences(string, letters))
-    print max(list_of_strings)
-    
+    print max(list_of_strings, key = len)
+
 # longest_two_character_substring('abbxcccc')
 # longest_two_character_substring('abcabcabcabccc')
 # longest_two_character_substring('qwertyytrewq')
+
