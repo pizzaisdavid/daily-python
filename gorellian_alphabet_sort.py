dba851747107657c0c1c26a9e81fd424b2c25b0e
pizzaisdavid <davidispizza@gmail.com>
2014-04-16 17:31:20 -0400

    Update gorellian_alphabet_sort.py

--- gorellian_alphabet_sort.py
+++ gorellian_alphabet_sort.py
@@ -1,12 +1,30 @@
-def initialize_variables(filename):
-    CONTENT = open(filename).readlines()
-    TEXT = list(''.join(CONTENT).replace('\n', ''))
-    word_count = TEXT[0]
-    board = TEXT[1:]
-    return player, board
+def gorellian_alphabet_sort(filename):
+    '''.'''
+    def initialize_variables(filename):
+        order = 'ZYXWVuTSRQpONMLkJIHGFEDCBa'.lower()
+        words = ['go', 'aLL', 'ACM', 'teamS', 'Go']
+        return words, order
+        
+    def is_alphabet(possible_alphabet):
+        '''.'''
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        for letter in alphabet:
+            if letter not in possible_alphabet:
+                return False
+        return True
+        
+    def label(order, words):
+        placement = {}
+        for word in words:
+            index = order.find(word[0].lower())
+            if index not in placement.keys():
+                placement[index] = [word]
+            else:
+                placement[index].append(word)
+        return placement
 
-def contains(iterable, required):
-    for x in required:
-        if x not in iterable:
-            return False
-    return True
+    words, order = initialize_variables('d')
+    if is_alphabet(order):
+        print sorted(label(order, words).keys())
+
+gorellian_alphabet_sort('d')
