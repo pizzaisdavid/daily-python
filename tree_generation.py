d81528c7d6ed4d348e59f0d3c140e8e703f8f82d
pizzaisdavid <davidispizza@gmail.com>
2014-04-18 19:32:50 -0400

    Update tree_generation.py

--- tree_generation.py
+++ tree_generation.py
@@ -1,1 +1,7 @@
 def tree(width, trunk, leaf):
+    number_of_spaces = width / 2
+    number_of_leafs = 1
+    for count in reversed(number_of_spaces):
+        print count * ' ' + number_of_leafs * leaf
+
+tree(7, '#', '*')
