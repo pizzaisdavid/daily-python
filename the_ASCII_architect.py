f87f4e569b7406b60e84bc2eeffca47bcf35bba3
pizzaisdavid <davidispizza@gmail.com>
2014-04-17 23:02:24 -0400

    Update the_ASCII_architect.py

--- the_ASCII_architect.py
+++ the_ASCII_architect.py
@@ -1,2 +1,9 @@
 def main():
+    def format_input(sequence):
+        number = '123456789'
+        for element in sequence:
+            if element in number:
+                
+        
     input_string = 'j3f3e3e3d3d3c3cee3c3c3d3d3e3e3f3fjij3f3f3e3e3d3d3c3cee3c3c3d3d3e3e3fj'
+    input_list = format_input(input_string)
