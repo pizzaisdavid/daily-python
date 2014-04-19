1e3d10e283f8b468a3b45590689df95230246d48
pizzaisdavid <davidispizza@gmail.com>
2014-04-18 22:52:14 -0400

    TRUNK_COUNT

--- tree_generation.py
+++ tree_generation.py
@@ -1,7 +1,12 @@
 def tree(width, trunk, leaf):
-    number_of_spaces = width / 2
-    number_of_leafs = 1
-    for count in reversed(number_of_spaces):
-        print count * ' ' + number_of_leafs * leaf
+    space_count = width / 2
+    TRUNK_COUNT = 3
+    leaf_count = 1
+    while space_count != 0:
+        print space_count * ' ' + leaf_count * leaf
+        leaf_count += 2
+        space_count -= 1
+    print ((width / 2) - 1) * ' ' + TRUNK_COUNT * trunk
 
 tree(7, '#', '*')
+tree(13, '=', '+')
