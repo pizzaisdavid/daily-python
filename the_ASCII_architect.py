f1a76e508e87d44b9c537a5f628d2868d3106ac6
pizzaisdavid <davidispizza@gmail.com>
2014-04-18 23:47:25 -0400

    Too many empty lines on the top of output

--- the_ASCII_architect.py
+++ the_ASCII_architect.py
@@ -12,12 +12,28 @@ def main():
         numbers = '123456789'
         letters = 'abcdefghij'
         wall = '++--***...'
-        for element in sequence:
-            if element in numbers:
+        for index, element in enumerate(sequence):
+            spaces = 0
+            s = wall[:letters.find(element) + 1]
+            if sequence[index - 1] in numbers:
+                s = int(sequence[index - 1]) * ' ' + s
+            if element in letters:
+                i = 0
+                s = s + '                      '
+            while i != len(house):
+                house[i].append(s[i])
+                i += 1
+        return house
+
+    def format_output(sequence):
+        for element in reversed(sequence):
+            print ''.join(element)
                 
-        
     input_string = 'j3f3e3e3d3d3c3cee3c3c3d3d3e3e3f3fjij3f3f3e3e3d3d3c3cee3c3c3d3d3e3e3fj'
     house = blank_house()
     input_list = format_input(input_string)
-    build_house(input_string, house)
-        
+    house = build_house(input_string, house)
+    format_output(house)
+
+main()
+
