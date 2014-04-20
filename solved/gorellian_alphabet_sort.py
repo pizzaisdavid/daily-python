8d33e3dfd5cdeac0d3888bffe03afd515b00d7cb
pizzaisdavid <davidispizza@gmail.com>
2014-04-20 02:15:18 -0400

    is_alphabet made simple

--- solved/gorellian_alphabet_sort.py
+++ solved/gorellian_alphabet_sort.py
@@ -8,10 +8,10 @@ def gorellian_alphabet_sort():
     def is_alphabet(possible_alphabet):
         '''Checks if the inputs contains all 26 letters.'''
         alphabet = 'abcdefghijklmnopqrstuvwxyz'
-        for letter in alphabet:
-            if letter not in possible_alphabet:
-                return False
-        return True
+        possible_alphabet = ''.join(sorted(possible_alphabet.lower()))
+        if possible_alphabet == alphabet:
+            return True
+        return False
             
     def group(order, sequence):
         dictionary = {}
@@ -22,23 +22,16 @@ def gorellian_alphabet_sort():
             dictionary[key].append(element)
         return dictionary
 
-    def length(x):
-        '''Takes a list that has a length of two and returns shorter, longer'''
-        if len(x[1]) < len(x[0]):
-            return x[1], x[0]
-        return x[0], x[1]
-
     def compare(order, words):
         if len(words) == 2:
-            shorter, longer = length(words)
-            for index, character in enumerate(shorter):
-                shorter_index = order.find(shorter[index].lower())
-                longer_index = order.find(shorter[index].lower())
-                if shorter_index > longer_index:
-                    return [shorter, longer]
-                elif shorter_index < longer_index:
-                    return [longer, shorter]
-            return [shorter, longer]
+            for index, character in enumerate(zip(words[0], words[1])):
+                index_1 = order.find(character[0].lower())
+                index_2 = order.find(character[1].lower())
+                if index_1 > index_2:
+                    return [words[0], words[1]]
+                elif index_1 < index_2:
+                    return [words[1], words[0]]
+            return [words[0], words[1]]
         return words
 
     def sorting(order, sequence):
@@ -48,18 +41,18 @@ def gorellian_alphabet_sort():
         return sequence
 
     def format_input(dictonary):
-        for key in sorted(dictonary.keys()):
+        for key in sorted(dictonary):
             for item in dictonary[key]:
                 print item
 
     words, order = initialize_variables()
     if is_alphabet(order):
         groups = group(order, words)
-        for key in sorted(groups.keys()):
+        for key in sorted(groups):
             groups[key] = sorting(order, groups[key])
         format_input(groups)
 
-gorellian_alphabet_sort()
+gorellian_alphabet_sort()    
 
 
 #/////
