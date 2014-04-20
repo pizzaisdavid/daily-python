a4ab954eab4e06af3992e18f56c72a7839c37149
pizzaisdavid <davidispizza@gmail.com>
2014-04-20 02:48:58 -0400

    cleaned code

--- solved/gorellian_alphabet_sort.py
+++ solved/gorellian_alphabet_sort.py
@@ -1,14 +1,8 @@
 def gorellian_alphabet_sort():
     '''Sorts words given a specific ordering.'''
-    def initialize_variables():
-        order = 'ZYXWVuTSRQpONMLkJIHGFEDCBa'.lower()
-        words = ['go', 'aLL', 'ACM', 'teamS', 'Go']
-        return words, order
-        
     def is_alphabet(possible_alphabet):
-        '''Checks if the inputs contains all 26 letters.'''
         alphabet = 'abcdefghijklmnopqrstuvwxyz'
-        possible_alphabet = ''.join(sorted(possible_alphabet.lower()))
+        possible_alphabet = ''.join(sorted(possible_alphabet))
         if possible_alphabet == alphabet:
             return True
         return False
@@ -24,20 +18,18 @@ def gorellian_alphabet_sort():
 
     def compare(order, words):
         if len(words) == 2:
-            for index, character in enumerate(zip(words[0], words[1])):
+            for character in zip(words[0], words[1]):
                 index_1 = order.find(character[0].lower())
                 index_2 = order.find(character[1].lower())
                 if index_1 > index_2:
                     return [words[0], words[1]]
                 elif index_1 < index_2:
                     return [words[1], words[0]]
-            return [words[0], words[1]]
         return words
 
     def sorting(order, sequence):
-        for d, s in enumerate(sequence):
-            for i, element in enumerate(sequence):
-                sequence[i: i+2] = compare(order, sequence[i: i+2])
+        for i, element in enumerate(sequence):
+            sequence[i: i+2] = compare(order, sequence[i: i+2])
         return sequence
 
     def format_input(dictonary):
@@ -45,15 +37,15 @@ def gorellian_alphabet_sort():
             for item in dictonary[key]:
                 print item
 
-    words, order = initialize_variables()
+    words = ['ANTLER','ANY','COW','HILL','HOW','HOWEVER','WHATEVER','ZONE']
+    order = 'UVWXYZNOPQRSTHIJKLMABCDEFG'.lower()
     if is_alphabet(order):
         groups = group(order, words)
         for key in sorted(groups):
             groups[key] = sorting(order, groups[key])
         format_input(groups)
 
-gorellian_alphabet_sort()    
-
+gorellian_alphabet_sort()
 
 #/////
 #   THIS CODE WORKS
