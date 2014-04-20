b70d5c4caf7445ae6606480a8daea51d8ed6e380
pizzaisdavid <davidispizza@gmail.com>
2014-04-20 03:14:10 -0400

    is_unique

--- solved/the_torn_number.py
+++ solved/the_torn_number.py
@@ -1,5 +1,8 @@
+def is_unique(x):
+    return len(set(string)) == len(string)
+
 for element in range(1000, 10000):
     string = str(element)
     squared = (int(string[0:2]) + int(string[2:4])) ** 2
-    if element == squared and set(string) == string:
+    if element == squared and is_unique(string):
         print element
