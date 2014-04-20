1400d8424eb5d0085d66e9181bae133e58c01941
pizzaisdavid <davidispizza@gmail.com>
2014-04-19 23:09:05 -0400

    "Transforming Code into Beautiful, Idiomatic Python"

--- general/group.py
+++ general/group.py
@@ -1,1 +1,11 @@
-def group(
+def group(sequence):
+    """
+    Groups a list by a characteristic and puts it an a dictionary.
+    """
+    dictionary = {}
+    for element in sequence:
+        key = len(elment) # Characteristic we are grouping by.
+        if key not in dictionary:
+            dictonary[key] = []
+        dictonary[key].append(element)
+    return dictonary
