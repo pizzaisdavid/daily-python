5d6b12ec974e26859fc5461d49ff3a18213c5e2a
pizzaisdavid <davidispizza@gmail.com>
2014-04-19 22:52:43 -0400

    used for loop

--- solved/tree_generation.py
+++ solved/tree_generation.py
@@ -1,12 +1,9 @@
 def tree(width, trunk, leaf):
-    space_count = width / 2
-    TRUNK_COUNT = 3
-    leaf_count = 1
-    while space_count != 0:
-        print space_count * ' ' + leaf_count * leaf
-        leaf_count += 2
-        space_count -= 1
-    print ((width / 2) - 1) * ' ' + TRUNK_COUNT * trunk
+    count = width / 2
+    TRUNK_WIDTH = 3
+    for index in reversed(range(count + 1)):
+        print index * ' ' + (width - 2 * index) * leaf
+    print (count - 1) * ' ' + TRUNK_WIDTH * trunk
 
 tree(7, '#', '*')
 tree(13, '=', '+')
