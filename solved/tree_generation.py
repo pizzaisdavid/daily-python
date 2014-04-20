ad47c8875c668277179fc2ddb995d17316182540
pizzaisdavid <davidispizza@gmail.com>
2014-04-19 23:34:34 -0400

    index to n

--- solved/tree_generation.py
+++ solved/tree_generation.py
@@ -1,8 +1,8 @@
 def tree(width, trunk, leaf):
     count = width / 2
     TRUNK_WIDTH = 3
-    for index in reversed(range(count + 1)):
-        print index * ' ' + (width - 2 * index) * leaf
+    for n in reversed(range(count + 1)):
+        print n * ' ' + (width - 2 * n) * leaf
     print (count - 1) * ' ' + TRUNK_WIDTH * trunk
 
 tree(7, '#', '*')
