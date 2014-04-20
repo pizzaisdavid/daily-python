e11cb8c88198515c078dcba47bffb65410cb8fc8
pizzaisdavid <davidispizza@gmail.com>
2014-04-19 23:42:00 -0400

    Update the_torn_number.py

--- solved/the_torn_number.py
+++ solved/the_torn_number.py
@@ -1,5 +1,5 @@
-    for element in range(1000, 10000):
-        string = str(element)
-        combined = int(string[0:2]) + int(string[2:4])
-        if element == combined ** 2 and len(set(string)) == len(string):
-            print element
+for element in range(1000, 10000):
+    string = str(element)
+    combined = int(string[0:2]) + int(string[2:4])
+    if element == combined ** 2 and len(set(string)) == len(string):
+        print element
