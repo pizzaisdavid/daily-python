6dc354a84ad1c226b1e578c1c471d6a88d327802
pizzaisdavid <davidispizza@gmail.com>
2014-04-26 00:41:32 -0400

    made a 'game' function and cleaned code

--- python/3.3/solved/RPSLS_part3.py
+++ python/3.3/solved/RPSLS_part3.py
@@ -1,6 +1,14 @@
 from random import randint
 
-def game():
+def main():
+    
+    def get_input(options, stop=['']):
+        combine = options + stop
+        choice = input('Enter: ').lower()
+        while choice not in combine:
+            choice = input(options).lower()
+        return choice
+        
     def introduction(options):
         print ('WELCOME TO ROCK, PAPER, SCISSORS, SPOCK, LIZARD')
         print ('Which AI would you like to play against? [random/learning/counter]')
@@ -12,14 +20,7 @@ def game():
         print (' ')
         return AI_TYPE
     
-    def get_input(options, stop=['']):
-        combine = options + stop
-        choice = input('Enter: ').lower()
-        while choice not in combine:
-            choice = input(options).lower()
-        return choice
-    
-    def AI(rules, played, tied, AI_TYPE):
+    def AI(rules, played, tied, AI_TYPE, options):
     
         def highest(dictonary):
             return max(dictonary, key=lambda x: x[0])
@@ -45,78 +46,72 @@ def game():
                 return tied[1]
             return counter_counters[highest(played)]
         
-        options = list(rules.keys())
         if AI_TYPE == 'random':
             return random(options)
         elif AI_TYPE == 'learning':
             return learning(rules, played, tied)
         return counter(rules, played, tied)
-
-    def RPSLS(rules, human, score, tied, AI_TYPE):
         
+    def game(AI_TYPE, options):
         def occurrences(sequence, find):
-            for index, element in enumerate(sequence):
-                if element == find:
-                    return False, index
-            return True, None
-
-        human_wins, computer_wins, ties = score
-        computer = AI(rules, played, tied, AI_TYPE)
-        print ('player pick: ' + human)
-        print ('computer pick: ' + computer)
-        computer_is_winner, c_index = occurrences(rules[human][0], computer)
-        player_is_winner, p_index = occurrences(rules[computer][0], human)
-        if human == computer:
-           print ('tie')
-           return (human_wins, computer_wins, ties + 1), human, (True, human)
-        elif computer_is_winner:
-            winner = computer
-            move = rules[computer][1][p_index]
-            loser = human
-            who = 'computer'
-            computer_wins += 1
-        else:
-            winner = human
-            move = rules[human][1][c_index]
-            loser = computer
-            who = 'human'
-            human_wins += 1
-        print (winner, move, loser, who, 'wins!', sep=' ')
-        print (' ')
-        return (human_wins, computer_wins, ties), human, (False, '')
-
-    def percentage(numerator, denominator):
-        decimal_place = 2
-        percent = numerator / denominator * 100
-        return str(round(percent, decimal_place)) + '%'
+                for index, element in enumerate(sequence):
+                    if element == find:
+                        return False, index
+                return True, None
+                
+        stop = ['exit', 'stop']
+        played = {'lizard': 0, 'spock': 0, 'paper': 0, 'scissors': 0, 'rock': 0}
+        tied = (False, '')
+        score = {'human_wins': 0, 'computer_wins': 0, 'ties': 0}
+        rules = {
+            # 'option': (['things it beats'], ['attack'], ['it loses to'])
+            'rock': (['scissors', 'lizard'], ['crushes', 'crushes'], ['paper', 'spock']),
+            'paper': (['spock', 'rock'], ['disproves', 'covers'], ['scissors', 'lizard']),
+            'scissors': (['paper', 'lizard'], ['cuts', 'decapitates'], ['rock', 'spock']),
+            'spock': (['rock', 'scissors'], ['vaporizes', 'smashes'], ['paper', 'lizard']),
+            'lizard': (['spock', 'paper'], ['poisons', 'eats'], ['rock', 'scissors'])
+            }
+        while True:
+            human = get_input(options, stop)
+            computer = AI(rules, played, tied, AI_TYPE, options)
+            if human in stop:
+                break
+            print ('player pick: ' + human)
+            print ('computer pick: ' + computer)
+            computer_is_winner, computer_index = occurrences(rules[human][0], computer)
+            player_is_winner, player_index = occurrences(rules[computer][0], human)
+            if human == computer:
+               print ('tie')
+               score['ties'] += 1
+               tied = (True, human)
+            elif computer_is_winner:
+                attack = rules[computer][1][player_index]
+                print (computer, attack, human, 'computer wins!', sep=' ')
+                score['computer_wins'] += 1
+            else:
+                attack = rules[human][1][computer_index]
+                print (human, attack, computer, 'human wins!', sep=' ')
+                score['human_wins'] += 1
+            print (' ')
+        return score
         
-    score = [0, 0, 0]
-    rules = {
-        # 'option': (['things it beats'], ['moves'], ['it loses to'])
-        'rock': (['scissors', 'lizard'], ['crushes', 'crushes'], ['paper', 'spock']),
-        'paper': (['spock', 'rock'], ['disproves', 'covers'], ['scissors', 'lizard']),
-        'scissors': (['paper', 'lizard'], ['cuts', 'decapitates'], ['rock', 'spock']),
-        'spock': (['rock', 'scissors'], ['vaporizes', 'smashes'], ['paper', 'lizard']),
-        'lizard': (['spock', 'paper'], ['poisons', 'eats'], ['rock', 'scissors'])
-        }
-    options = list(rules.keys())
-    stop = ['exit', 'stop']
-    played = {'lizard': 0, 'spock': 0, 'paper': 0, 'scissors': 0, 'rock': 0}
-    human = 'rock'
-    tied = (False, '')
+    def scoreboard(score):
+        
+        def percentage(numerator, denominator):
+            decimal_places = 2
+            percent = numerator / denominator * 100
+            return str(round(percent, decimal_places)) + '%'
+            
+        total = sum(score.values())
+        print ('~~~~~~~~FINAL~SCORE~~~~~~~~')
+        print ('TIES:', score['ties'], percentage(ties, total), sep=' ')
+        print ('HUMAN:', score['human_wins'], percentage(human_wins, total), sep=' ')
+        print ('COMPUTER(' + AI_TYPE + '):', score['computer_wins'], percentage(computer_wins, total), sep=' ')
+        print ('~~~~~~~~FINAL~SCORE~~~~~~~~')
+        
+    options = ['lizard', 'spock', 'paper', 'scissors', 'rock']
     AI_TYPE = introduction(options)
-    while True:
-        human = get_input(options, stop)
-        if human in stop:
-            break
-        score, human, tied = RPSLS(rules, human, score, tied, AI_TYPE)
-        played[human] += 1
-    human_wins, computer_wins, ties = score
-    total = sum(score)
-    print ('~~~~~~~~FINAL~SCORE~~~~~~~~')
-    print ('TIES:', ties, percentage(ties, total), sep=' ')
-    print ('HUMAN:', human_wins, percentage(human_wins, total), sep=' ')
-    print ('COMPUTER(' + AI_TYPE + '):', computer_wins, percentage(computer_wins, total), sep=' ')
-    print ('~~~~~~~~FINAL~SCORE~~~~~~~~')
+    score = game(AI_TYPE, options)
+    scoreboard(score)
 
-game()
+main()
