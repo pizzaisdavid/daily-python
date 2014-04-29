23b24e7892116ebb2631132bdcf02d70980a6773
pizzaisdavid <davidispizza@gmail.com>
2014-04-28 20:51:06 -0400

    Update trigonometric_triangle_trouble_part1.py

--- python/3.3/trigonometric_triangle_trouble_part1.py
+++ python/3.3/trigonometric_triangle_trouble_part1.py
@@ -1,1 +1,12 @@
-def main()
+def main():
+    triangle = triangle()
+    print (triangle.a)
+  
+class Triangle:
+    __init__(self, *args):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.A = A
+        self.B = B
+        self.C = C
