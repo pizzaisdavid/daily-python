88d56b6e397570784c829ad071257bb362e79306
pizzaisdavid <davidispizza@gmail.com>
2014-04-29 23:49:07 -0400

    solve for angle

--- python/3.3/trigonometric_triangle_trouble_part1.py
+++ python/3.3/trigonometric_triangle_trouble_part1.py
@@ -1,10 +1,28 @@
 def main():
     triangle = Triangle()
     print (triangle.a)
-  
+    
+    while triangle.is_solved == False:
+        if triangle.unknown_side_count == 1:
+            pythagorean_theorem()
+        elif triangle.unknown_side_count == 1:
+            solve_for_angle(triangle.angle)
+            
 class Triangle:
     __init__(self, *args):
         self.sides = [a, b, c]
         self.angles = [A, B, C]
-
+        self.is_solved = False
+        
+def solve_for_angle(angles):
+    unknown_index = occurrences(angles, 0)
+    unknown_angle = 180
+    for angle in angles:
+        unknown_angle -= angle
+    angles[unknown_index] = unknown_angle
+    return angles
+    
+def occurrences(sequence, find):
+    return [i for i, x in enumerate(sequence) if x == find]
+    
 main()
