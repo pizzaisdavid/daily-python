a869e4114bbc26f7af1fde9e5790c335dc2dc2c8
pizzaisdavid <davidispizza@gmail.com>
2014-04-30 00:11:21 -0400

    path-thag.

--- python/3.3/trigonometric_triangle_trouble_part1.py
+++ python/3.3/trigonometric_triangle_trouble_part1.py
@@ -6,23 +6,29 @@ def main():
         if triangle.unknown_side_count == 1:
             pythagorean_theorem()
         elif triangle.unknown_side_count == 1:
-            solve_for_angle(triangle.angle)
+            solve_for_an_angle(triangle.angle)
             
 class Triangle:
-    __init__(self, *args):
+    def __init__(self, *args):
         self.sides = [a, b, c]
         self.angles = [A, B, C]
         self.is_solved = False
+
+def pythagorean_theorem(sides):
+    square_root = math.sqrt()
+    OPEN = 0
+    if sides[2] == OPEN:
+        sides[2] = square_root(side[0]**2 + side[1]**2)
+    elif:
+
+    return sides
         
-def solve_for_angle(angles):
-    unknown_index = occurrences(angles, 0)
-    unknown_angle = 180
+def solve_for_an_angle(angles):
+    OPEN = 0
+    unknown = 180
     for angle in angles:
-        unknown_angle -= angle
-    angles[unknown_index] = unknown_angle
+        unknown -= angle
+    angles[angles.index(OPEN)] = unknown
     return angles
     
-def occurrences(sequence, find):
-    return [i for i, x in enumerate(sequence) if x == find]
-    
-main()
+print (solve_for_angle([34, 0, 90]))
