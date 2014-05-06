1e84ec34c7ba557a6b29e1657395362461f5f9fd
pizzaisdavid <davidispizza@gmail.com>
2014-05-05 23:15:03 -0400

    throws errors sometimes.

--- python/3.3/blackjack.py
+++ python/3.3/blackjack.py
@@ -1,29 +1,47 @@
 from random import randint
 def black_jack(number_of_decks):
-	number_of_cards = 2
-	cards = deal(number_of_decks, number_of_cards)
+	score = deal(initialize_deck(number_of_decks))
+	total = score.wins + score.loses
+	print 'After', total, 'hands there was', score.wins, 'at', percent(score.wins, total)
 
-def deal(number_of_decks, number_of_cards):
-	NUMBER_OF_CARDS_IN_A_DECK = 52
-	deck = initialize_deck(number_of_decks)
+def deal(deck):
+	score = reset_count()
 	while deck:
-		indices = draw_two(deck)
-	print (cards)
+		hand = deck.pop(random(deck)) + deck.pop(random(deck))
+		hand, deck = hit_me_again(hand, deck)
+		score = check(hand, score)
+	return score
 
 def initialize_deck(number_of_decks):
-	deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+	cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 	NUMBER_OF_SUITS = 4
-	return deck * NUMBER_OF_SUITS * number_of_decks
+	return cards * NUMBER_OF_SUITS * number_of_decks
 
-def draw_two(deck):
-	indices = []
-	while len(indices) < 2:
-		indices.append(randint(0, len(deck)))
-		set(indices)
-	return indices
+class reset_count:
+	def __init__(self, wins=0, loses=0):
+		self.wins = wins
+		self.loses = loses
 
+def random(sequence):
+	return randint(0, len(sequence) - 1)
 
-def cut(sequence, index, length=1):
-	return sequence[:index] + sequence[index + length:]
+def check(value, score):
+	WINNER = 21
+	if value == WINNER:
+		score.wins += 1
+	else:
+		score.loses += 1
+	return score
+
+def hit_me_again(hand, deck):
+	LIMIT = 11
+	if deck and hand <= LIMIT:
+		hand += deck.pop(random(deck))
+	return hand, deck
+
+def percent(numerator, denominator):
+    percent = numerator / float(denominator) * 100
+    DECIMAL_PLACES = 2
+    return str(round(percent, DECIMAL_PLACES)) + '%'
 
 black_jack(2)
