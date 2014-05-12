eb70e1faec20a49444953d11cc13b0ea4395e46a
pizzaisdavid <davidispizza@gmail.com>
2014-05-12 00:00:59 -0400

    percent function changed to a variable.

--- python/3.3/solved/blackjack.py
+++ python/3.3/solved/blackjack.py
@@ -1,19 +1,19 @@
 from random import randint
 
 def black_jack(number_of_decks):
-    deck = shuffle(initialize_deck(number_of_decks))
+    deck = shuffle(create_deck(number_of_decks))
     wins = 0
-    loses = 0
+    total = 0
     while deck:
         try:
             hand = deck.pop() + deck.pop()
             hand, deck = hit_me_again(hand, deck)
-            wins, loses = score(hand, wins, loses)
+            wins, total = score(hand, wins, total)
         except IndexError:
             break
-    print_output(wins, loses)
+    print_output(wins, total)
 
-def initialize_deck(number_of_decks):
+def create_deck(number_of_decks):
     cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
     NUMBER_OF_SUITS = 4
     return cards * NUMBER_OF_SUITS * number_of_decks
@@ -32,19 +32,15 @@ def hit_me_again(hand, deck):
         hand += deck.pop()
     return hand, deck
 
-def score(value, wins, loses):
+def score(value, wins, total):
     WINNER = 21
     if value == WINNER:
-        return wins + 1, loses
-    return wins, loses + 1
+        wins += 1
+    return wins, total + 1
 
-def print_output(wins, loses):
-    total = wins + loses
-    print 'After', total, 'hands there was', wins, 'at', percent(wins, total)
-
-def percent(numerator, denominator):
-    percent = numerator / float(denominator) * 100
+def print_output(wins, total):
     DECIMAL_PLACES = 2
-    return str(round(percent, DECIMAL_PLACES)) + '%'
+    percent = round(wins / float(total) * 100, DECIMAL_PLACES)
+    print 'After', total, 'hands there was', wins, 'at', percent, '%'
 
 black_jack(2)
