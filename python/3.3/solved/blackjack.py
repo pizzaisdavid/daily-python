3df5096fad6ec5d192be70625e700e9b3382c2c5
pizzaisdavid <davidispizza@gmail.com>
2014-05-12 23:38:20 -0400

    longest function is 10 lines.

--- python/3.3/solved/blackjack.py
+++ python/3.3/solved/blackjack.py
@@ -2,26 +2,24 @@ from random import randint
 
 def black_jack(number_of_decks):
     deck = create_and_shuffle_deck(number_of_decks)
-    wins = 0
-    total = 0
+    wins, total = 0, 0
     while deck:
         try:
             hand = deck.pop() + deck.pop()
             hand, deck = hit_me_again(hand, deck)
-            wins += score(hand)
-            total += 1
+            wins, total = wins + score(hand), total + 1
         except IndexError:
             break
     print_output(wins, total)
 
 def create_and_shuffle_deck(number_of_decks):
-    cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+    CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
     NUMBER_OF_SUITS = 4
-    return shuffle(cards * NUMBER_OF_SUITS * number_of_decks)
+    return shuffle(CARDS * NUMBER_OF_SUITS * number_of_decks)
 
 def shuffle(deck):
-    length = len(deck)
-    for count in range(length):
+    LENGTH = len(deck)
+    for count in range(LENGTH):
         random_card = deck.pop(randint(0, length - 1))
         deck.append(random_card)
     return deck
@@ -39,8 +37,8 @@ def score(value):
     return 0
 
 def print_output(wins, total):
-    DECIMAL_PLACES = 2
-    percent = round(wins / float(total) * 100, DECIMAL_PLACES)
-    print 'After', total, 'hands there was', wins, 'at', percent, '%'
+    DECIMAL_PLACE = 2
+    percent = str(round(wins / float(total) * 100, DECIMAL_PLACE)) + '%'
+    print 'After', total, 'hands there was', wins, 'at', percent
 
 black_jack(2)
