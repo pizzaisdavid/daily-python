690eb19e530728f1c2f1bbdec2927a396450a538
pizzaisdavid <davidispizza@gmail.com>
2014-05-13 00:35:21 -0400

    Update sublime_settings.py

--- general/sublime_settings.py
+++ general/sublime_settings.py
@@ -2,14 +2,14 @@
 	"color_scheme": "Packages/Color Scheme - Default/Twilight.tmTheme",
 	"font_size": 13,
 	"translate_tabs_to_spaces": true,
-  "draw_minimap_border": true,
-  "always_show_minimap_viewport": true,
-  "highlight_line": true,
-  "caret_style": "phase",
+	"draw_minimap_border": true,
+	"always_show_minimap_viewport": true,
+	"highlight_line": true,
+	"caret_style": "phase",
 	"rulers": [70],
-  "trim_trailing_white_space_on_save": true,
-  "auto_complete_commit_on_tab": true,
-  "auto_complete_delay": 50,
-  "show_full_path": false,
-  "scroll_past_end": false
+	"trim_trailing_white_space_on_save": true,
+  	"auto_complete_commit_on_tab": true,
+  	"auto_complete_delay": 50,
+  	"show_full_path": false,
+  	"scroll_past_end": false
 }
