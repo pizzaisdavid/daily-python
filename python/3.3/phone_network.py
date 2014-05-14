96e8266bd68d93af4fed32bdd1731e3e9caa5e0f
pizzaisdavid <davidispizza@gmail.com>
2014-05-14 00:44:37 -0400

    Update phone_network.py

--- python/3.3/phone_network.py
+++ python/3.3/phone_network.py
@@ -1,39 +1,31 @@
 def phone_network(filename):
-    network, calls = parse_input(filename)
-    network = create_network(network)
-    for call in calls:
-        network = place_call(network, call)
+	network, calls = parse_input(filename)
+	network = create_network(network)
+	print network
+	print look_up(network, ('C', 'E'))
 
 def parse_input(filename):
-    items = {'network':[], 'calls':[]}
-    key = 'network'
-    for line in open(filename).readlines():
-        item = line.split()
-        if item == []:
-            key = 'calls'
-            continue
-        items[key].append(item)
-    return items['network'], items['calls']
+	items = {'network':[], 'calls':[]}
+	key = 'network'
+	for line in open(filename).readlines():
+		item = line.split()
+		if item == []:
+			key = 'calls'
+			continue
+		items[key].append(item)
+	return items['network'], items['calls']
 
 def create_network(connections):
-    dictonary = skeleton_network(connections)
-    for start, end, unit in connections:
-        unit = int(unit)
-        dictonary[end].append((start, unit))
-        dictonary[start].append((end, unit)) # allows for two sided web
-    return dictonary
+	web = {}
+	for connection in connections:
+		unit, start, end = sorted(connection)
+		if start not in web:
+			web[start] = {}
+		web[start][end] = int(unit)
+	return web
 
-def skeleton_network(connections):
-    dictonary = {}
-    for key, location, _ in connections:
-        dictonary[key] = []
-        dictonary[location] = [] # allows for two sided web
-    return dictonary
-
-def place_call(network, call):
-    path = build_call(netowrk, call)
-
-def build_call(network, call):
-    start, end = call
+def look_up(network, call):
+	start, end = sorted(call)
+	return network[start][end]
 
 phone_network('phone_network.txt')
