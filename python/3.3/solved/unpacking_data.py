42867f3c31041f7fc9324679307cb6a5b1c4a9ac
pizzaisdavid <davidispizza@gmail.com>
2014-05-17 00:41:02 -0400

    parse_input and general restructuring

--- python/3.3/solved/unpacking_data.py
+++ python/3.3/solved/unpacking_data.py
@@ -1,36 +1,33 @@
 def decompress(filename):
     KEYWORDS, compress = parse_input(filename)
     for physical_line in compress:
-        translate(KEYWORDS, physical_line)
+        translate_line(KEYWORDS, physical_line)
 
 def parse_input(filename):
-    KEYWORDS, compress = [], []
-    key = 'KEYWORDS'
-    for line in open(filename).readlines():
-        if line.split() == []:
-            key = 'compress'
-            continue
-        if key is 'KEYWORDS':
-            KEYWORDS.append(line.rstrip())
-        else:
-            compress.append(line.rstrip().split(' '))
-    return KEYWORDS[1:], compress
+    lines = map(lambda x: x.strip(), open(filename).readlines())
+    INDEX_KEYWORDS_END = int(lines[0]) + 1
+    KEYWORDS = lines[1: INDEX_KEYWORDS_END]
+    compress = format_compress(lines[INDEX_KEYWORDS_END:])
+    return KEYWORDS, compress
 
-def translate(KEYWORDS, physical_line):
+def format_compress(lines):
+    return map(lambda x: x.split(' '), lines)
+
+def translate_line(KEYWORDS, physical_line):
     string = ''
     for chunk in physical_line:
-        string = convert(KEYWORDS, chunk, string)
+        string = chunk_to_keyword(KEYWORDS, chunk, string)
 
-def convert(KEYWORDS, chunk, string):
+def chunk_to_keyword(KEYWORDS, chunk, string):
     if has_modifier(chunk):
-        string += chunk_has_modifier(KEYWORDS, chunk)
+        string += add_keyword_with_modifier(KEYWORDS, chunk)
     elif is_keyword(chunk):
         string += add_keyword(KEYWORDS, chunk)
     elif is_symbol(chunk):
         string = add_symbol(string, chunk)
     elif is_linebreak(chunk):
         print (string.replace('- ', '-'))
-        return  ''
+        return ''
     return string + ' '
 
 def has_modifier(possibly_contains_modifier):
@@ -40,7 +37,7 @@ def has_modifier(possibly_contains_modifier):
             return True
     return False
 
-def chunk_has_modifier(KEYWORDS, command):
+def add_keyword_with_modifier(KEYWORDS, command):
     CAPS_LOCK, CAPITALISED = '!', '^'
     index, modifier = unpack(command)
     string = add_keyword(KEYWORDS, index)
