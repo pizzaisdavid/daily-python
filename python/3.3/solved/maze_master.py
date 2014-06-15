bfed02c24035ad74a535d9f4017b63a0304a91d3
pizzaisdavid <davidispizza@gmail.com>
2014-06-14 23:04:39 -0400

    Update and rename python/3.3/maze_master.py to python/3.3/solved/maze_master.py

--- python/3.3/solved/maze_master.py
+++ python/3.3/solved/maze_master.py
@@ -1,18 +1,18 @@
 def maze_master(filename):
     maze = setup(filename)
-    player = Player(maze)
+    position = Position(maze)
     WALL = '#'
     DEAD_END = 'B'
     EXPLORED = '*'
     UNEXPLORED = ' '
     EXIT = 'E'
-    while player.is_in_maze():
-        if player.at_dead_end(DEAD_END + WALL):
-            player.mark_as(DEAD_END)
+    while position.is_in_maze():
+        if position.at_dead_end(DEAD_END + WALL):
+            position.mark_as(DEAD_END)
         else:
-            player.mark_as(EXPLORED)
-        player.move(UNEXPLORED, EXPLORED, EXIT)
-    player.print_maze(DEAD_END)
+            position.mark_as(EXPLORED)
+        position.move(UNEXPLORED, EXPLORED, EXIT)
+    position.print_path(DEAD_END)
     print('done')
 
 def setup(filename):
@@ -23,10 +23,10 @@ def setup(filename):
         grid.append(line)
     return grid
 
-class Player:
+class Position:
     def __init__(self, maze):
         self.maze = maze
-        self.horizontal, self.vertical = Player.locate(self, 'S')
+        self.horizontal, self.vertical = Position.locate(self, 'S')
 
     def locate(self, symbol):
         horizontal, vertical = 0, 0
@@ -40,17 +40,21 @@ class Player:
 
     def is_in_maze(self):
         EXIT = 'E'
-        return self.maze[self.vertical][self.horizontal] != EXIT
+        row = self.maze[self.vertical]
+        cell = row[self.horizontal]
+        return cell != EXIT
 
     def at_dead_end(self, WALL):
         directions_blocked = 0
         DEAD_END = 3
         START = 'S'
-        Player.get_surrounding_content(self)
+        Position.get_surrounding_content(self)
         for cell in self.content:
             if cell in WALL:
                 directions_blocked += 1
-        if self.maze[self.vertical][self.horizontal] == START:
+        row = self.maze[self.vertical]
+        cell = row[self.horizontal]
+        if cell == START:
             return False
         return directions_blocked == DEAD_END
 
@@ -66,16 +70,21 @@ class Player:
         self.content = content
 
     def mark_as(self, SYMBOL):
-        self.maze[self.vertical][self.horizontal] = SYMBOL
+        PROTECTED = 'S'
+        row = self.maze[self.vertical]
+        cell = row[self.horizontal]
+        if cell not in PROTECTED:
+            self.maze[self.vertical][self.horizontal] = SYMBOL
 
     def move(self, UNEXPLORED, EXPLORED, EXIT):
-        Player.get_dictonary_surrounding(self)
-        if UNEXPLORED in self.adjacent.values():
-            Player.move_to(self, UNEXPLORED)
-        elif EXIT in self.adjacent.values():
-            Player.move_to(self, EXIT)
-        elif EXPLORED in self.adjacent.values():
-            Player.move_to(self, EXPLORED)
+        Position.get_dictonary_surrounding(self)
+        adjacent = self.adjacent.values()
+        if UNEXPLORED in adjacent:
+            Position.move_to(self, UNEXPLORED)
+        elif EXIT in adjacent:
+            Position.move_to(self, EXIT)
+        elif EXPLORED in adjacent:
+            Position.move_to(self, EXPLORED)
 
     def get_dictonary_surrounding(self):
         DIRECTIONS = ['up', 'right', 'down', 'left']
@@ -85,7 +94,7 @@ class Player:
         self.adjacent = adjacent
 
     def move_to(self, SYMBOL):
-        x, y = Player.get_direction(self, SYMBOL)
+        x, y = Position.get_direction(self, SYMBOL)
         self.horizontal += x
         self.vertical += y
 
@@ -99,7 +108,7 @@ class Player:
             if self.adjacent[key] == SYMBOL:
                 return directions[key]
 
-    def print_maze(self, DEAD_END):
+    def print_path(self, DEAD_END):
         for row in self.maze:
             line = ''.join(row).replace(DEAD_END, ' ')
             print(line)
