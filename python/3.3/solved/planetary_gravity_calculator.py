a441dfee954ab1d2fe8acca1af00717cd901a62b
pizzaisdavid <davidispizza@gmail.com>
2014-06-18 00:19:14 -0400

    Update planetary_gravity_calculator.py

--- python/3.3/solved/planetary_gravity_calculator.py
+++ python/3.3/solved/planetary_gravity_calculator.py
@@ -1,33 +1,35 @@
 from math import pi as PI
 
 def calculator(filename):
-    object_mass, statistics = setup(filename)
-    find = Find(object_mass)
+    mass, statistics = setup(filename)
+    find = Find(mass)
     DECIMAL_PLACE = 3
     for statistic in statistics:
         name, _, _ = statistic
         force = find.weight(statistic)
-        weight = round(force, DECIMAL_PLACE)
-        print(name, ': ', weight, sep='')
+        force = round(force, DECIMAL_PLACE)
+        print(name, ': ', force, sep='')
    
 def setup(filename):
     mass, count, *planets = open(filename)
     statistics = []
     for planet in planets:
         name, radius, density = planet.split(',')
-        statistics.append([name, int(radius), int(density)])
+        radius = int(radius)
+        density = int(density)
+        statistics.append([name, radius, density])
     return int(mass), statistics
 
 class Find:
-    def __init__(self, object_mass):
-        self.object_mass = object_mass
+    def __init__(self, mass):
+        self.mass = mass
 
     def weight(self, statistic):
         G = 6.67e-11
         name, radius, density = statistic
         volume = 4/3 * PI * radius**3
-        planet_mass = volume * density
-        force = G * self.object_mass * planet_mass / radius**2
+        mass = volume * density
+        force = G * self.mass * mass / radius**2
         return force
 
 calculator('planets.txt')
