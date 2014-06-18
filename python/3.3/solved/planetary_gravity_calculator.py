10eb2508c5d0949fe3ee4e94c1a80c1b855460b3
pizzaisdavid <davidispizza@gmail.com>
2014-06-18 01:01:21 -0400

    Update planetary_gravity_calculator.py

--- python/3.3/solved/planetary_gravity_calculator.py
+++ python/3.3/solved/planetary_gravity_calculator.py
@@ -1,35 +1,42 @@
 from math import pi as PI
 
 def calculator(filename):
-    mass, statistics = setup(filename)
-    find = Find(mass)
+    object_mass, statistics = setup(filename)
+    calculate = Calculate(object_mass)
     DECIMAL_PLACE = 3
     for statistic in statistics:
-        name, _, _ = statistic
-        force = find.weight(statistic)
-        force = round(force, DECIMAL_PLACE)
-        print(name, ': ', force, sep='')
+        name, radius, density = statistic
+        force = calculate.weight(radius, density)
+        print(name, ': ', round(force, DECIMAL_PLACE), sep='')
    
 def setup(filename):
-    mass, count, *planets = open(filename)
+    object_mass, count, *planets = open(filename)
     statistics = []
     for planet in planets:
         name, radius, density = planet.split(',')
-        radius = int(radius)
-        density = int(density)
-        statistics.append([name, radius, density])
-    return int(mass), statistics
+        statistics.append([name, int(radius), int(density)])
+    return int(object_mass), statistics
 
-class Find:
-    def __init__(self, mass):
-        self.mass = mass
+class Calculate:
+    def __init__(self, object_mass):
+        self.object_mass = object_mass
 
-    def weight(self, statistic):
+    def weight(self, radius, density):
+        self.radius = radius
+        self.density = density
+        Calculate.volume(self)
+        Calculate.mass(self)
+        return Calculate.force(self)
+    
+    def volume(self):
+        self.volume = 4/3 * PI * self.radius**3
+
+    def mass(self):
+        self.mass = self.volume * self.density
+
+    def force(self):
         G = 6.67e-11
-        name, radius, density = statistic
-        volume = 4/3 * PI * radius**3
-        mass = volume * density
-        force = G * self.mass * mass / radius**2
-        return force
-
+        distance = self.radius**2
+        return G * self.object_mass * self.mass / distance
+         
 calculator('planets.txt')
