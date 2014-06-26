6f0f1c0f2122f15cd7f0b5ea6abdcc21543af8d4
pizzaisdavid <davidispizza@gmail.com>
2014-06-26 01:46:47 -0400

    score

--- general/percent.py
+++ general/percent.py
@@ -2,3 +2,9 @@ def percent(numerator, denominator):
     percent = numerator / float(denominator) * 100
     DECIMAL_PLACES = 2
     return str(round(percent, DECIMAL_PLACES)) + '%'
+    
+def percentage(score):
+    DECIMAL_PLACE = 2
+    CONVERT_TO_PERCENT = 100
+    percent = score.wins / float(score.total) * CONVERT_TO_PERCENT
+    return round(percent, DECIMAL_PLACE)
