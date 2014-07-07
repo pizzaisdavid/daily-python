c11f8f6602a95a2c64ba53139ea9a9023efb5bf6
pizzaisdavid <davidispizza@gmail.com>
2014-07-07 01:26:00 -0400

    sub-functions for abstraction layers

--- python/3.3/solved/rotate_2D_array.py
+++ python/3.3/solved/rotate_2D_array.py
@@ -1,38 +1,43 @@
-    def rotate(filename):
-        DIMENSION, grid = setup(filename)
-        TURNS = [90, 180, 270]
-        for turn in TURNS:
-            indices = get_shifts(DIMENSION)
-            grid = translate(grid, indices)
-            output(DIMENSION, turn, grid)
+def rotate(filename):
+    DIMENSION, grid = setup(filename)
+    TURNS = [90, 180, 270]
+    for turn in TURNS:
+        new_indices = translate(DIMENSION)
+        grid = sync(grid, new_indices)
+        output(DIMENSION, turn, grid)
 
-    def setup(filename):
-        grid = []
-        n, *array = open(filename)
-        for line in array:
-            row = line.split()
-            grid.extend(row)
-        return int(n), grid
+def setup(filename):
+    grid = []
+    width, *array = open(filename)
+    for row in array:
+        grid.extend(row.split())
+    return int(width), grid
 
-    def get_shifts(DIMENSION):
-        LARGEST = DIMENSION * (DIMENSION - 1)
-        indices = []
-        for height in range(DIMENSION):
-            shift = LARGEST + height
-            for width in range(DIMENSION):
-                indices.append(shift)
-                shift -= DIMENSION
-        return indices
+def translate(DIMENSION):
+    TOP_LEFT_CORNER = DIMENSION * (DIMENSION - 1)
+    indices = []
+    for horizontal in range(DIMENSION):
+        shift = TOP_LEFT_CORNER + horizontal
+        indices.extend(get_vertical_shifts(DIMENSION, shift))
+    return indices
 
-    def translate(grid, indices):
-        return [grid[index] for index in indices]
+def get_vertical_shifts(DIMENSION, shift):
+    shifts = []
+    for vertical in range(DIMENSION):
+        shifts.append(shift)
+        shift -= DIMENSION
+    return shifts
 
-    def output(DIMENSION, turn, grid):
-        print(turn)
-        for width in range(DIMENSION):
-            row = grid[width * DIMENSION: (width + 1) * DIMENSION]
-            print(' '.join(row))
-        print(' ')
-        
-            
-    rotate('rotate.txt')
+def sync(grid, indices):
+    return [grid[index] for index in indices]
+
+def output(DIMENSION, turn, grid):
+    print(turn)
+    for iterator in range(DIMENSION):
+        print(get_row(DIMENSION, grid, iterator))
+    print(' ')
+
+def get_row(length, grid, iterator):
+    return ' '.join(grid[iterator * length: (iterator + 1) * length])
+          
+rotate('rotate.txt')
