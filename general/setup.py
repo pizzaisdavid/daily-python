e8ee369991ffe4a8d345dea3530e7349a045d4df
pizzaisdavid <davidispizza@gmail.com>
2014-07-09 19:17:17 -0400

    Update setup.py

--- general/setup.py
+++ general/setup.py
@@ -1,1 +1,8 @@
 def setup(filename):
+    grid = []
+    with open(filename) as file:
+        length, *datas = file
+        for data in datas:
+            grid.extend(data.split())
+    return int(length), grid
+            
