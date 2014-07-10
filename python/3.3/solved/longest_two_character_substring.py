5fbc993614bf4bc927c358058dd824e45540780c
pizzaisdavid <davidispizza@gmail.com>
2014-07-09 23:23:41 -0400

    removed is_in() function

--- python/3.3/solved/longest_two_character_substring.py
+++ python/3.3/solved/longest_two_character_substring.py
@@ -1,28 +1,23 @@
 def longest_two_character_substring(string):
     substrings = []
-    for pair in get_unique_pairs(string):
-        substrings.extend(get_substrings_made_up_of_a_group(string, pair))
+    for pair in get_unique_combinations(string):
+        substrings.extend(get_substrings_of_consecutive_letters(string, pair))
     print(max(substrings, key=len))
 
-def get_unique_pairs(sequence):
-    pairs = get_n_length_character_combinations(sequence, length=2)
-    return set(pairs) & set(map(reformat, pairs))
+def get_unique_combinations(sequence):
+    combinations = get_n_length_character_combinations(sequence)
+    return set(combinations) & set(map(reformat, combinations))
 
-def get_n_length_character_combinations(sequence, length):
+def get_n_length_character_combinations(sequence, length=2):
     return [sequence[i: i + length] for i, _ in enumerate(sequence)]
 
 def reformat(string):
     return ''.join(sorted(string))
 
-def get_substrings_made_up_of_a_group(string, group):
-    SPLITTER = ', '
-    substrings = [is_in(SPLITTER, group, character) for character in string]
-    return ''.join(substrings).split(SPLITTER)
-
-def is_in(SPLITTER, check, character):
-    if character in check:
-        return character
-    return SPLITTER
+def get_substrings_of_consecutive_letters(string, group):
+    SPLIT = ', '
+    substrings = [letter if letter in group else SPLIT for letter in string]
+    return ''.join(substrings).split(SPLIT)
 
 longest_two_character_substring('abbccc')
 longest_two_character_substring('abcabcabcabccc')
