c3113dea66cb742df37cb1c67ed0541d29f86485
pizzaisdavid <davidispizza@gmail.com>
2014-08-06 16:17:26 -0400

    Update ask.py

--- general/ask.py
+++ general/ask.py
@@ -1,1 +1,6 @@
-def ask():
+def ask(options, prompt):
+    answer = input(prompt).lower()
+    while answer not in options:
+        answer = input('Please enter in one of the following: {0}'.format(options))
+    return answer
+    
