81e15805d27f806c389b3c675ee07b7ea4d51062
pizzaisdavid <davidispizza@gmail.com>
2014-08-12 15:30:37 -0400

    Lowers the strings

--- python/3.3/solved/bogo_sort.py
+++ python/3.3/solved/bogo_sort.py
@@ -1,12 +1,15 @@
 import random
 
 def bogo(scrambled, match):
-    scrambled = list(scrambled)
-    MATCH = list(match)
+    scrambled, MATCH = reformat(scrambled), reformat(match)
     count = 0
     while scrambled != MATCH:
         random.shuffle(scrambled)
         count += 1
-    print('ITERATIONS: {0}'.format(count))
+    print('ITERATIONS: ', count)
+
+def reformat(string):
+    return list(string.lower())
 
 bogo('lolhe', 'hello')
+
