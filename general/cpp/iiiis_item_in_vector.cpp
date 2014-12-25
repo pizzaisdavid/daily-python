276f8a94f77cc7e6ddae429500d31e3af1fc5f3c
David Germain <davidispizza@gmail.com>
2014-12-24 19:05:31 -0500

    Delete iiiis_item_in_vector.cpp

--- general/cpp/iiiis_item_in_vector.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
-bool is_item_in_vector(vector<string> sequence, string item) {
-    return find(sequence.begin(), sequence.end(), item) != sequence.end();
-}
